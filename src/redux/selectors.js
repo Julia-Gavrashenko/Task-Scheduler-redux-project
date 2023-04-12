41fa807a4629fe6a3fba276420516b3a7b4d8a50
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-04-12 18:01:25 +0300

    add-git-actions

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,3 +2,7 @@ export const selectTasks = state => state.tasks.items;
 export const selectIsLoading = state => state.tasks.isLoading;
 export const selectError = state => state.tasks.error;
 export const selectStatusFilter = state => state.filters.status;
+
+
+
+
